678550e6187a38a796ec43be62fda8fc0959fc38
DaichiSaito <hrkedz@gmail.com>
2019-10-12 11:04:11 +0900

    [Add]ちょっとした仕様変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      auto_login(@user)
       redirect_to login_path, success: 'ユーザーを作成しました'
     else
       flash.now[:danger] = 'ユーザーの作成に失敗しました'
