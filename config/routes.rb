afd389be9e2f44092b9eecd50548c4837eae7bf9
yuki0509 <hara.0509@icloud.com>
2020-06-30 15:15:40 +0900

    annotate使用

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
+# == Route Map
+#
+#                    Prefix Verb   URI Pattern                                                                              Controller#Action
+#                      root GET    /                                                                                        sessions#new
+#                     login GET    /login(.:format)                                                                         sessions#new
+#                           POST   /login(.:format)                                                                         sessions#create
+#                    logout DELETE /logout(.:format)                                                                        sessions#destroy
+#                     users GET    /users(.:format)                                                                         users#index
+#                           POST   /users(.:format)                                                                         users#create
+#                  new_user GET    /users/new(.:format)                                                                     users#new
+#        rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
+# rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
+#        rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
+# update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
+#      rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
+
 Rails.application.routes.draw do
   root to: 'sessions#new'
   get '/login', to: 'sessions#new'
