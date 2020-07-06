64d8554c5fbbe51f822f03b3b70c1de23765b537
yuki0509 <hara.0509@icloud.com>
2020-07-06 11:16:26 +0900

    bootstrap-meterial-design追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-//= require jquery3
-//= require popper
+//= require jquery
+//= require popper.js
 //= require rails-ujs
 //= require bootstrap-material-design/dist/js/bootstrap-material-design.js
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_15_124413) do
+ActiveRecord::Schema.define(version: 2020_06_26_033004) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
