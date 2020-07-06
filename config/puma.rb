0ba9870bfde466ebf1123e849bf0172bc3b437e4
yuki0509 <hara.0509@icloud.com>
2020-07-06 10:54:23 +0900

    コンフリクト解消

--- config/puma.rb
+++ config/puma.rb
@@ -3,12 +3,8 @@
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
-#
-<<<<<<< HEAD
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-=======
+
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
->>>>>>> daison_clone/feature/01_login_logout
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
