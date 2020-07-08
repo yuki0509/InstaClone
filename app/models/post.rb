5aa9e7ffd5395ddc3fa73536f1b75978e4006b55
yuki0509 <hara.0509@icloud.com>
2020-07-08 14:16:24 +0900

    バリデーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   validates :body, presence: true
+  validates :images, presence: true
   #imagesカラムにImageUploaderをマウントさせる。carrierwaveの実装には欠かせない作業。
   mount_uploaders :images, ImageUploader
 
