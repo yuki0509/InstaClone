ff0f241901c0d527194bb11262bc5667af896e17
yuki0509 <hara.0509@icloud.com>
2020-07-09 23:07:57 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,5 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users, only: %i[new create]
+  resources :posts
 end
