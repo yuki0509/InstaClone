63b83b15f7a318dd33b1d0db547f2763daffb71d
yuki0509 <hara.0509@icloud.com>
2020-07-16 11:34:33 +0900

    バリデーションの追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -20,6 +20,8 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class Comment < ApplicationRecord
+  validates :body, presence: true, length: { maximum: 1000 }
+  
   belongs_to :user
   belongs_to :post
 end
