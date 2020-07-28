4ad20957867ee82adcef83ecb799a90dc93f2844
yuki0509 <hara.0509@icloud.com>
2020-07-28 16:32:41 +0900

    検索フォームに代入するインスタンス変数を作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  # 各アクションの前に@search_formを作っておく。
+  before_action :set_search_posts_form
   # flashオブジェクトで利用できるキーを追加する
   add_flash_types :success, :info, :warning, :danger
 
@@ -6,4 +8,16 @@ class ApplicationController < ActionController::Base
   def not_authenticated
     redirect_to login_path, warning: 'ログインしてください'
   end
+
+  private
+  # 検索フォームはナビゲーションバーに共通して、設置してあるため、ApplicationControllerにメソッドをかく。　
+  def set_search_posts_form
+    @search_form = SearchPostsForm.new(search_post_params)
+  end
+  
+  def search_post_params
+    # fetchメソッド。params[:q]の値がないなら、{}を取ってくる。
+    params.fetch(:q, {}).permit(:body)
+  end
+  
 end
