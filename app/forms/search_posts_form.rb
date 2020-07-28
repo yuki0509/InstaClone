4a9c6ea048e0ce55caa0d66bf4e4a15fba18c4b1
yuki0509 <hara.0509@icloud.com>
2020-07-28 17:00:46 +0900

    検索フォームをフォームオブジェクトで実装

---
+++ app/forms/search_posts_form.rb
@@ -0,0 +1,19 @@
+class SearchPostsForm
+  # DBを使用せずに、ActiveRecordと同じ挙動をする。（バリデーションやエラーメッセージの表示ができる）
+  include ActiveModel::Model
+  # attributeメソッドが使えるようになる。オブジェクトの属性を型指定して使えるようにすることができる。
+  include ActiveModel::Attributes
+  # body属性を追加する。ActiveRecordを使用した時と同じように扱えるので。bodyカラムとしても扱える。
+  attribute :body, :string
+  
+  def search
+    # ここでのbodyはSearchPostFormのオブジェクトの属性。attribute :bodyでオブジェクトにbody属性が追加された。
+    scope = if self.body.present?
+              # 重複なく投稿を取得する。この場合は、postオブジェクトのidが違うため重複してるオブジェクトはなく、allメソッドとしていることは同じ。
+              Post.distinct.body_contain(self.body)
+            else
+              # 重複なく投稿を取得
+              Post.distinct
+            end
+  end
+end
