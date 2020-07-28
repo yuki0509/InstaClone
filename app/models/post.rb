98db25a929643b62e06064d5ac8c6467b8b8fdd7
yuki0509 <hara.0509@icloud.com>
2020-07-28 16:05:09 +0900

    postモデルにscope追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,6 +18,9 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class Post < ApplicationRecord
+  # 引数wordと部分一致するbodyを検索する。('body LIKE ?', "%#{word}%")では?に%word%が代入され、Like検索ができる。
+  scope :body_contain, ->(word){ where('body LIKE ?', "%#{word}%") }
+
   validates :body, presence: true, length: { maximum: 1000 }
   validates :images, presence: true
   # imagesカラムにアップローダークラスをマウントする。carrierwaveの実装にはこの作業が必要。
