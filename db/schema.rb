3937106337b30bd84199f6055cfa8d34f21421d2
yuki0509 <hara.0509@icloud.com>
2020-07-30 15:10:31 +0900

    avatarカラムの変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_21_131502) do
+ActiveRecord::Schema.define(version: 2020_07_30_054747) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "body", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_07_21_131502) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
