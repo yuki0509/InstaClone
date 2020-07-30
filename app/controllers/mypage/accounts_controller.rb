c91d43abee9f0f63da3e87ecab3439ebd6d42999
yuki0509 <hara.0509@icloud.com>
2020-07-30 14:31:30 +0900

    フォーム機能実装

--- app/controllers/mypage/accounts_controller.rb
+++ app/controllers/mypage/accounts_controller.rb
@@ -1,7 +1,20 @@
 class Mypage::AccountsController < ApplicationController
   def edit
+    @user_account = current_user
   end
 
   def update
+    @user_account = current_user
+    if @user_account.update(user_account_params)
+      redirect_to edit_mypage_account_path, success: 'プロフィールを更新しました'
+    else
+      render :edit
+    end
   end
+
+  private
+  def user_account_params
+    params.require(:user).permit(:name)
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,10 +21,10 @@ class User < ApplicationRecord
   # sorceryで追加された
   authenticates_with_sorcery!
 
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || crypted_password_chabged? }
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   # これがないとpassword_confirmation属性が追加されない
-  validates :password, confirmation: true, if: -> { new_record? || crypted_password_chabged? }
-  validates :password_confirmation, presence: true, if: -> { new_record? || crypted_password_chabged? }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :email, presence: true, uniqueness: true
 
   # userが削除されたら、関連するpostも削除される
