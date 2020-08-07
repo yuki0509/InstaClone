07ab1ae817fd8d8cf8e172f6cd967fe4a2b4b9eb
yuki0509 <hara.0509@icloud.com>
2020-08-07 21:46:56 +0900

    いいね機能のメール通知機能実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,6 +3,7 @@ class LikesController < ApplicationController
     # urlのクエリからパラメーターとして受け取る。
     @post = Post.find(params[:post_id])
     current_user.like(@post)
+    UserMailer.with(user_from: current_user, user_to: @post.user, post: @post).like_post.deliver_later
   end
 
   def destroy
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,11 @@
 class UserMailer < ApplicationMailer
+  default from: "instaclone@example.com"
+  def like_post
+    @user_from = params[:user_from]
+    @user_to = params[:user_to]
+    @post = params[:post]
+    mail(to: @user_to.email, subject: "#{@user_from.name}があなたの投稿にいいねしました")
+  end
+  
+
 end
